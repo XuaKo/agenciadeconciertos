0a8a922ead0df5541ea62f93258b43028f4aa451
german bango <92051496+germanbango@users.noreply.github.com>
2021-11-06 22:48:03 +0100

    Update Descuento.java

--- agenciadeconciertos/src/entidades/Descuento.java
+++ agenciadeconciertos/src/entidades/Descuento.java
@@ -1,5 +1,29 @@
 package entidades;
+import java.time.LocalDate;
 
 public class Descuento {
+  private LocalDate Fecha_Validez;
+	private long Codigo_Unico; 
+	
+	public LocalDate getFecha_Validez() 
+	{
+		return Fecha_Validez;
+	}
+	
+	public void setFecha_Validez(LocalDate fecha_validez)
+	{
+		Fecha_Validez = fecha_validez;
+	}
+	
+	public long getCodigo_Unico()
+	{
+		return Codigo_Unico;
+	}
+	
+	public void setCodigo_Unico(long codigo_unico)
+	{
+		Codigo_Unico = codigo_unico; 
+	}
+	
 
 }
