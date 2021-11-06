d35d209bad436c3d2101a778d229725ccdd4f4ba
german bango <92051496+germanbango@users.noreply.github.com>
2021-11-06 22:46:02 +0100

    Update Artista.java

--- agenciadeconciertos/src/entidades/Artista.java
+++ agenciadeconciertos/src/entidades/Artista.java
@@ -1,5 +1,39 @@
 package entidades;
 
 public class Artista {
+private long Identificador;
+	private String Nombre_Artistico;
+	private long Genero;
+	
+	
+	public long getidentIdenficador() 
+	{
+		return Identificador;		
+	}
+	
+	public void setidentIdenficador(long identificador)
+	{
+		Identificador = identificador;
+	}
+	
+	public String getNombre_Artistico() 
+	{
+		return Nombre_Artistico;
+	}
+	
+	public void setNombre_Artistico(String nombre_artistico)
+	{
+		Nombre_Artistico = nombre_artistico;
+	}
+	
+	public long getGenero()
+	{
+		return Genero;
+	}
+	
+	public void setGenero(long genero)
+	{
+		Genero = genero;
+	}
 
 }
