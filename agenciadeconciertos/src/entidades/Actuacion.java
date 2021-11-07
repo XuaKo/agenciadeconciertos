dcc2a95bb074b10cd096e4faf7f1392949298369
german bango <92051496+germanbango@users.noreply.github.com>
2021-11-07 13:09:31 +0100

    Update Actuacion.java

--- agenciadeconciertos/src/entidades/Actuacion.java
+++ agenciadeconciertos/src/entidades/Actuacion.java
@@ -1,5 +1,38 @@
 package entidades;
 
 public class Actuacion {
+  private long Identificacion;
+  private int Numero_Secuencia;
+  private String Duracion;
+  
+  public long getIdentificacion()
+  {
+    return Identificacion;
+  }
+  
+  public void setIdentificacion(long identificacion)
+  {
+    Identificacion = identificacion;
+  }
+ 
+  public int getNumero_Secuencia()
+  {
+    return Numero_Secuencia;
+  }
+  
+  public void setNumero_Secuencia(int numero_secuencia)
+  {
+    Numero_Secuencia = numero_secuencia;
+  }
+  
+  public String getDuracion()
+  {
+    return Duracion;
+  }
+  
+  public void setDuracion(String duracion)
+  {
+    Duracion = duracion;
+  }
 
 }
