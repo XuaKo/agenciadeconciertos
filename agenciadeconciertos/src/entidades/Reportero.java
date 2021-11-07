ba3a3f7bec49d057e67c99c4a9b7f18a947938b1
german bango <92051496+germanbango@users.noreply.github.com>
2021-11-07 14:42:25 +0100

    Update Reportero.java

--- agenciadeconciertos/src/entidades/Reportero.java
+++ agenciadeconciertos/src/entidades/Reportero.java
@@ -1,5 +1,38 @@
 package entidades;
 
 public class Reportero {
+  private String NombreyApellidos;
+  private String NIF;
+  private int telefono;
+  public String getNombreyApellidoos()
+  {
+    return NombreyApellidos;
+  }
 
+  public void setNombreyApellidos(Strin nombreyapellidos)
+  {
+    NombreyApellidos = nombreyapellidos;
+  }
+  
+  public String getNIF()
+  {
+    return NIF;
+  }
+  
+  public void setNIF(String nif)
+  {
+    NIF = nif; 
+  }
+  
+  public int gettelefono()
+  {
+      retrun Telefono;
+  }
+  
+  public void setTelefono(int telefono)
+  {
+    Telefono = telefono;
+  }
+  
+  
 }
