aaa625561c1b0312c15b2270a6ce7bac35783784
german bango <92051496+germanbango@users.noreply.github.com>
2021-11-07 11:52:49 +0100

    Update Entrada.java

--- agenciadeconciertos/src/entidades/Entrada.java
+++ agenciadeconciertos/src/entidades/Entrada.java
@@ -1,5 +1,38 @@
 package entidades;
 
 public class Entrada {
+  private long Identificador;
+  private String Precio_Euro;
+  private boolean Vip;
+  
+  public long getIdentificador()
+  {
+    return Identificador;
+  }  
+  
+  public void setIdentificador(long identificador)
+  {
+    Identificador = identificador;
+  }
+  
+  public String getPrecio_Euro()
+  {
+    return Precio_Euro;
+  }
+  public void setPrecio_Euro(String precio_euro)
+  {
+    Precio_Euro = precio_euro;
+  }
+  
+  public boolean getVip()
+  {
+    return Vip;
+  }
+  
+  public void setVip(boolean vip)
+  {
+    Vip = vip;
+  }
 
+  
 }
