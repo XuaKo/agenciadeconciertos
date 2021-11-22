30c19645259b488cbc111b8ad1db064a6179e206
germanbango <ivu19876@educastur.es>
2021-11-22 17:07:22 +0100

    Class Generar_Arrays

--- agenciadeconciertos/src/entidades/Generar_Arrays.java
+++ agenciadeconciertos/src/entidades/Generar_Arrays.java
@@ -1,6 +1,34 @@
 package entidades;
 import java.util.ArrayList;
 
-public class Generar_Arrays {
+public class Generar_Arrays<A> {
+	public A obj;
+	
+	public Generar_Arrays() {
+		
+	}
 
+	 public Generar_Arrays(A o) {
+	        obj = o;
+	    }
+
+	    public A get() {
+	        return obj;
+	    }
+
+	    public void set(A a) {
+	        this.obj = a;
+	    }
+
+	    public void classType() {
+	        System.out.println("El tipo de clase es " + obj.getClass().getName());
+	    }
+
+	    public final ArrayList<A> convertir(A[] array) {
+	        ArrayList<A> ret = new ArrayList<A>();
+	        for (A a : array) {
+	            ret.add((A) a);
+	        }
+	        return ret;
+	    }
 }
