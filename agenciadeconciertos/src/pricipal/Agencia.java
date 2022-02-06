61d1ea1f83afb5a984e40e9dc98f3bde54001e52
gabriel-Fernandez-Melendez <yic00732@educastur.es>
2022-02-06 11:38:14 +0100

    vamos a subir cosas

--- agenciadeconciertos/src/pricipal/Agencia.java
+++ agenciadeconciertos/src/pricipal/Agencia.java
@@ -6,28 +6,33 @@ public class Agencia {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			Scanner teclado = new Scanner(System.in);
-			boolean validarMP=false;
-			int select = teclado.nextInt();
-		
-			do {
-				System.out.println("MENU PRINCIPAL");
-				System.out.println("1: Gestion de Giras");
-				System.out.println("2: Gestion de Conciertos");
-				System.out.println("3: Gestion de Reporteros");
-				System.out.println("4: Gestion de Artistas");
-				System.out.println("5: Salir");
-				switch (select) {
-				
-				case 1: 
-					boolean validarGG=false;
-					int selectG = teclado.nextInt();
+
+		boolean val = false;
+		Scanner scan = new Scanner(System.in);
+
+		do {
+			System.out.println("MENU PRINCIPAL");
+			System.out.println("1: Gestion de Giras");
+			System.out.println("2: Gestion de Conciertos");
+			System.out.println("3: Gestion de Reporteros");
+			System.out.println("4: Gestion de Artistas");
+			System.out.println("5: Salir");
+
+			// declaramos las variables para inicar las primeras elecciones del menu
+
+			int elecc = scan.nextInt();
+
+			switch (elecc) {
+
+			case 1:
+				boolean validarGG = false;
 				do {
 					System.out.println("Gestion de Giras");
 					System.out.println("1: Añadir una Gira");
 					System.out.println("2: Eliminar una Gira");
 					System.out.println("3: Modificar una Gira");
 					System.out.println("4: Volver al menu principal");
+					int selectG = scan.nextInt();
 					switch (selectG) {
 					case 1:
 						break;
@@ -36,15 +41,15 @@ public class Agencia {
 					case 3:
 						break;
 					case 4:
-						validarGG=true;
+						validarGG = true;
 						break;
 					}
-				
-					}while (!validarGG);
-					break;
-				case 2:
-					boolean validarGC=false;
-					int selectC = teclado.nextInt();
+
+				} while (!validarGG);
+				break;
+			case 2:
+				boolean validarGC = false;
+				int selectC = scan.nextInt();
 				do {
 					System.out.println("Gestion de Conciertos");
 					System.out.println("1: Añadir un Concierto");
@@ -58,15 +63,15 @@ public class Agencia {
 					case 3:
 						break;
 					case 4:
-						validarGC=true;
+						validarGC = true;
 						break;
 					}
-				
-					}while (!validarGC);
-					break;
-				case 3:
-					boolean validarGR=false;
-					int selectR = teclado.nextInt();
+
+				} while (!validarGC);
+				break;
+			case 3:
+				boolean validarGR = false;
+				int selectR = scan.nextInt();
 				do {
 					System.out.println("Gestion de Reporteros");
 					System.out.println("1: Añadir un Reportero");
@@ -80,15 +85,15 @@ public class Agencia {
 					case 3:
 						break;
 					case 4:
-						validarGR=true;
+						validarGR = true;
 						break;
 					}
-				
-					}while (!validarGR);
-					break;
-				case 4:
-					boolean validarGA=false;
-					int selectA = teclado.nextInt();
+
+				} while (!validarGR);
+				break;
+			case 4:
+				boolean validarGA = false;
+				int selectA = scan.nextInt();
 				do {
 					System.out.println("Gestion de Artistas");
 					System.out.println("1: Añadir un Artista");
@@ -102,21 +107,20 @@ public class Agencia {
 					case 3:
 						break;
 					case 4:
-						validarGA=true;
+						validarGA = true;
 						break;
 					}
-				
-					}while (!validarGA);
-					break;
-				case 5:
-					validarMP=true;
-					break;
-				default:
-					break;
-			}
-			}while(!validarMP);
-		
-			teclado.close();
-			}
-			}
 
+				} while (!validarGA);
+				break;
+			case 5:
+				val = true;
+				break;
+			default:
+				break;
+			}
+		} while (!val);
+
+		scan.close();
+	}
+}
